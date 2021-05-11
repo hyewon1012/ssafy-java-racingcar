3ff65f02168cc6697828ef7cd030efb28830ce1c
김혜원 <akrnrh9811@gmail.com>
2021-05-11 23:47:47 +0900

    test:Set Collection 테스트 코드 작성

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,53 @@
+package step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set 크기 확인 테스트")
+    @Test
+    public void sizeCheck(){
+        int setSize = numbers.size();
+        assertThat(numbers).hasSize(setSize);
+    }
+
+    @DisplayName("Set Value 확인 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    public void contains(int value){
+        assertThat(numbers.contains(value)).isTrue();
+
+    }
+
+    @DisplayName("Set에 존재하지않는 value가 있는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"3:True","2:True","1:True","4:False","5:False"}, delimiter = ':')
+    public void containsAllCheck(int input, boolean expected){
+        boolean isContain = numbers.contains(input);
+        assertThat(expected).isEqualTo(isContain);
+
+    }
+}
