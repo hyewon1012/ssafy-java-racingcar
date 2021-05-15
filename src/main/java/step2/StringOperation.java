81fd4b40254ebe63101473733d0ce97d508099fe
김혜원 <akrnrh9811@gmail.com>
2021-05-15 22:13:27 +0900

    fix:사칙연산 메소드 기능 수정

--- src/main/java/step2/StringOperation.java
+++ src/main/java/step2/StringOperation.java
@@ -9,39 +9,54 @@ import java.util.stream.Stream;
 public final class StringOperation {
 
     private StringOperation() {}
-    private static class SingletonHelper {
+    private static final class SingletonHelper {
         public static final StringOperation INSTANCE = new StringOperation();
     }
     public static StringOperation getInstance(){
         return SingletonHelper.INSTANCE;
     }
 
-    public static int add(final List<String> operand) {
-        return operand.stream()
-                .map(s -> s.charAt(0))
-                .filter(Character::isDigit)
-                .mapToInt(c -> c -'0')
-                .reduce(0, Integer::sum);
-    }
-
-    public static int subtract(final List<String> operand) {
-        return operand.stream()
-                .map(s -> s.charAt(0))
-                .filter(Character::isDigit)
-                .mapToInt(c -> c - '0')
-                .reduce((a,b) -> a-b).getAsInt();
-    }
-
-    public static int multiply(final List<String> operand) {
+    private static int calculateFourOperation(int a, int b, char operator){
+        switch (operator){
+            case '+': return add(a, b);
+            case '-': return subtract(a, b);
+            case '*': return multiply(a, b);
+            case '/': return divide(a, b);
+        }
         return 0;
     }
 
-    public static int divide(final List<String> operand) {
-        return 0;
+    public final static int calculate(String input){
+        int b = 0;
+        char operator = ' ';
+        int result = 0;
+
+        List<String> operand = Arrays.asList(input.split(" "));
+
+        for (int i = 0; i < operand.size()-2; i+=2){
+            result = Integer.parseInt(operand.get(i));
+            b = Integer.parseInt(operand.get(i+2));
+            operator = operand.get(i+1).charAt(0);
+
+            result = calculateFourOperation(result, b, operator);
+        }
+        return result;
     }
 
-
-    public static int operate(final List<String> operand){
-        return 0;
+    public static int add(final int a, final int b) {
+        return a+b;
     }
+
+    public static int subtract(final int a, final int b) {
+        return a-b;
+    }
+
+    public static int multiply(final int a, final int b) {
+        return a*b;
+    }
+
+    public static int divide(final int a, final int b) {
+        return a/b;
+    }
+
 }
--- src/test/java/step2/StringOperationTest.java
+++ src/test/java/step2/StringOperationTest.java
@@ -22,11 +22,9 @@ public class StringOperationTest {
     @CsvSource(value = {"2 + 3:5", "6 + 9:15", "0 + 1:1"}, delimiter = ':')
     void 덧셈(String input, Integer expected){
         //when
-        List<String> operand = Arrays.asList(input.split(" "));
         StringOperation stringOperation = StringOperation.getInstance();
-
+        int addResult = stringOperation.calculate(input);
         //then
-        int addResult = stringOperation.add(operand);
         assertThat(addResult).isEqualTo(expected);
     }
 
@@ -35,36 +33,32 @@ public class StringOperationTest {
     @CsvSource(value = {"5 - 3:2", "6 - 9:-3", "1 - 1:0"}, delimiter = ':')
     void 뺄셈(String input, Integer expected){
         //when
-        List<String> operand = Arrays.asList(input.split(" "));
         StringOperation stringOperation = StringOperation.getInstance();
-
+        int subtractResult = stringOperation.calculate(input);
         //then
-        int subtractResult = stringOperation.subtract(operand);
         assertThat(subtractResult).isEqualTo(expected);
     }
 
     @DisplayName("곱셈 테스트")
-    @Test
-    void 곱셈(){
-        //given
-        String input = "5 * 3";
+    @ParameterizedTest(name = "{0} 결과는 {1} 입니다.")
+    @CsvSource(value = {"5 * 3:15", "6 * 9:54", "1 * 0:0"}, delimiter = ':')
+    void 곱셈(String input, Integer expected){
         //when
-        List<String> operand = Arrays.asList(input.split(" "));
+        StringOperation stringOperation = StringOperation.getInstance();
+        int multiplyResult = stringOperation.calculate(input);
         //then
-        int multiplyResult = StringOperation.multiply(operand);
-        assertThat(multiplyResult).isEqualTo(15);
+        assertThat(multiplyResult).isEqualTo(expected);
     }
 
     @DisplayName("나눗셈 테스트")
-    @Test
-    void 나눗셈(){
-        //given
-        String input = "5 / 3";
+    @ParameterizedTest(name = "{0} 결과는 {1} 입니다.")
+    @CsvSource(value = {"5 / 3:1", "12 / 6:2", "25 / 5:5"}, delimiter = ':')
+    void 나눗셈(String input, Integer expected){
         //when
-        List<String> operand = Arrays.asList(input.split(" "));
+        StringOperation stringOperation = StringOperation.getInstance();
+        int divideResult = stringOperation.calculate(input);
         //then
-        int divideResult = StringOperation.divide(operand);
-        assertThat(divideResult).isEqualTo(1);
+        assertThat(divideResult).isEqualTo(expected);
     }
 
     @DisplayName("입력값이 null 이거나 빈 공백 문자일 경우")
@@ -74,23 +68,20 @@ public class StringOperationTest {
         List<String> operand = Arrays.asList(input.split(" "));
         //when and then
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            StringOperation.operate(operand);
+            StringOperation.calculate(input);
         });
     }
 
     @DisplayName("잘못된 사칙연산 기호 사용")
     @Test
-    void 잘못된연산기호사용(){
-        //given
-        String input = "3 // 4 ++ 5";
-        List<String> operand = Arrays.asList(input.split(" "));
-
+    @ValueSource(strings = {"3 ** 5", "1 -- 7", "4 ?? 4"})
+    void 잘못된연산기호사용(String input){
         /*//when
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> StringOperation.operate(operand));*/
 
         //when and then
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            StringOperation.operate(operand);
+            StringOperation.calculate(input);
         }).withMessageContaining("잘못된 연산 기호");
     }
 
@@ -99,10 +90,9 @@ public class StringOperationTest {
     void 사칙연산(){
         //given
         String input = "2 + 3 * 4 / 2";
-        List<String> operand = Arrays.asList(input.split(" "));
 
         //when
-        int result = StringOperation.operate(operand);
+        int result = StringOperation.calculate(input);
 
         //then
         assertThat(result).isEqualTo(10);
