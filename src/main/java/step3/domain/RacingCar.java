d7e2b930b86e43727236021452184f7f18942a87
김혜원 <akrnrh9811@gmail.com>
2021-06-08 00:54:58 +0900

    feat:멤버변수 초기화 코드 추가

--- src/main/java/step3/domain/RacingCar.java
+++ src/main/java/step3/domain/RacingCar.java
@@ -3,15 +3,19 @@ package step3.domain;
 import step3.strategy.MoveStrategy;
 
 public final class RacingCar {
+
+    private static final int INIT_TRACE_VALUE = 0;
+    private static final int MOVING_CONDITION = 4;
     private MoveStrategy moveStrategy;
     private int trace;
 
     public RacingCar(MoveStrategy moveStrategy){
         this.moveStrategy = moveStrategy;
+        this.trace = INIT_TRACE_VALUE;
     }
 
     public final int move(){
-        if(moveStrategy.move() >= 4){
+        if(moveStrategy.move() >= MOVING_CONDITION){
             this.trace++;
         }
         return this.trace;
