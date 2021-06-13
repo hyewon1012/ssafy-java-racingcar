58312cf9a0d04fa99d8f5c570c67929147bc6810
김혜원 <akrnrh9811@gmail.com>
2021-06-13 21:40:25 +0900

    feat:사용자 입력 형식 변경

--- src/main/java/step4/ui/InputView.java
+++ src/main/java/step4/ui/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public final class InputView {
     private static final String NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+    private static final String CAR_NAMES = "경주에 참여할 자동차 이름을 모두 입력해주세요";
     private static final String ROUND = "시도할 회수는 몇 회 인가요?";
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -14,6 +15,12 @@ public final class InputView {
     public static int enterNumberOfCar(){
         System.out.println(NUMBER_OF_CARS);
         return scanner.nextInt();
+    }
+
+    public static String[] enterCarNames(){
+        System.out.println(CAR_NAMES);
+        String[] names = scanner.nextLine().split(",");
+        return names;
     }
 
     public static int enterRound(){
